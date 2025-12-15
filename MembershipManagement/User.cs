001aa2f4854b4029d7ed9319b2a7620e7d1ed601
Myroslava DANKO <myroslava.danko@alten.com>
2025-12-15 16:03:35 +0100

    learn API and db building connecting + swagger

---
+++ MembershipManagement/User.cs
@@ -0,0 +1,10 @@
+// public class User
+// {
+//     public Guid Id { get; private set; }
+//     public string Email { get; private set; }
+
+//     public User(string email)
+//     {
+//         Email = email ?? throw new ArgumentNullException(nameof(email));
+//     }
+// }
